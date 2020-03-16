b244877249aafa3ecd469567271c297022121b1b
Michelle Sokolov <michellesokolov@Michelles-MacBook-Pro.local>
2020-03-16 13:47:06 -0700

    changes

---
+++ client/build/precache-manifest.46087f9eabeb0e667f5bb4801c6e0e28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "caada8859f75e34db774",
+    "url": "/static/css/main.dc35a01c.chunk.css"
+  },
+  {
+    "revision": "caada8859f75e34db774",
+    "url": "/static/js/main.9352db8b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e60aeacaa6cbfae40d65",
+    "url": "/static/js/2.b3d896ab.chunk.js"
+  },
+  {
+    "revision": "9a1da9b6fbadd611687a22f34d392af2",
+    "url": "/index.html"
+  }
+];
